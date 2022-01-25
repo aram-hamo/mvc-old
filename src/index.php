20928957ac098fed4457d3fe9f5b2d55a11b2f9e
Aram Hamo <contact@aramhamo.me>
2022-01-25 16:07:26 +0100

    modified:   index.php

--- src/index.php
+++ src/index.php
@@ -6,11 +6,11 @@ include(__DIR__.'/includes/autoload.php');
 if(!isset($_GET["url"])){
     include(__DIR__.'/views/'.$views['/'].'.php');
 
-}elseif(isset($views[$_GET["url"]])){
-    include(__DIR__.'/views/'.$views[$_GET["url"]].'.php');
+}elseif(isset($views['/'.$_GET["url"]])){
+    include(__DIR__.'/views/'.$views['/'.$_GET["url"]].'.php');
 
-}elseif(isset($views[$_GET["url"].'/'])){
-    include(__DIR__.'/views/'.$views[$_GET["url"].'/'].'.php');
+}elseif(isset($views['/'.$_GET["url"].'/'])){
+    include(__DIR__.'/views/'.$views['/'.$_GET["url"].'/'].'.php');
 }else{
     header('Location: /');
 }
