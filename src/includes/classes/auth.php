728c602b3fb861b14da66078acdc1767f575796e
Aram Hamo <contact@aramhamo.me>
2022-08-16 21:43:29 +0200

    modified:   includes/classes/auth.php 	modified:   install/tables.sql

--- src/includes/classes/auth.php
+++ src/includes/classes/auth.php
@@ -3,14 +3,17 @@
 class auth extends db{
 
   public function register($username,$password,$email){
-    
+
+  $tokan = bin2hex(openssl_random_pseudo_bytes(16));
   $hashedPassword = password_hash($password,PASSWORD_BCRYPT);
-	$createUser = $this->conn->prepare("insert into users (username,password,email,verified) values (:username ,:password,:email,false)");
+	$createUser = $this->conn->prepare("insert into users (username,password,email,verified,tokan) values (:username ,:password,:email,false,:tokan)");
 	$createUser->bindValue(':username',$username);
 	$createUser->bindValue(':email',$email);
 	$createUser->bindValue(':password',$hashedPassword);
-	$createUser->execute();
-
+  $createUser->bindValue(':tokan',$tokan);
+	if($createUser->execute()){
+    setcookie("tokan",$tokan);
+  }
   }
 
   public function login($username,$password){
@@ -19,7 +22,17 @@ class auth extends db{
   $stmt->bindValue(':username',$username);
   $stmt->execute();
   $userData =  $stmt->fetchAll();
-  return password_verify($password,$userData[0]['password']);
+  $verify = password_verify($password,$userData[0]['password']);
+  if($verify){
+    setcookie("tokan",$userData[0]['tokan']);
+  }
+  return $verify;
+  }
+  public function loginCheck(){
 
+  $stmt = $this->conn->prepare('select * from users where tokan=:tokan');
+  $stmt->bindValue(':tokan',$_COOKIE['tokan']);
+  $stmt->execute();
+  return $stmt->fetchAll();
   }
 }
--- src/install/tables.sql
+++ src/install/tables.sql
@@ -1,1 +1,1 @@
-create table if not exists users (id integer primary key autoincrement,username text unique, password text , email text,verified boolean)
+create table if not exists users (id integer primary key autoincrement,username text unique, password text , email text,verified boolean,tokan text unique)
